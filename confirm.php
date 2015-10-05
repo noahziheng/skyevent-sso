fa73cd78dcf2eeb2254936af9709cf101f273315
noahgaocn <ziheng1719@163.com>
2015-10-05 10:23:30 +0800

    update confirm.php

--- confirm.php
+++ confirm.php
@@ -7,14 +7,21 @@ require 'config.php';
 use Vatsim\OAuth\SSO;
 $sso = new SSO($base, $key, $secret, $method, $cert);
 // Outside Laravel
+$session = $_SESSION['vatsimauth'];
+
 $sso->validate(
-    $_POST['key'],
-    $_POST['secret'],
-    $_POST['oauth_verifier'],
+    $session['key'],
+    $session['secret'],
+    $_GET['oauth_verifier'],
     function($user, $request) {
         // At this point we can remove the session data.
-        //unset($_SESSION['vatsimauth']);
+        unset($_SESSION['vatsimauth']);
+
+        // do something to log the user in on your site using the user id
+        var_dump($user);
+
+        // Redirect home
+        //header('Location: /');
         //die();
-        echo json_encode($user);
     }
 );
