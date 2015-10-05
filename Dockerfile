27b6f6ed01545280fe552935176bcb62eb894739
Noah Gao <ziheng1719@163.com>
2015-10-05 17:54:57 +0800

    Initial Commit

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM php:5.6-apache
-COPY ./ /var/www/html/
--- config.php
+++ config.php
@@ -1,40 +1,66 @@
 <?php
-/*
- * DO NOT PUBLISH THE KEY, SECRET AND CERT TO CODE REPOSITORIES
- * FOR SECURITY.
+/**
+ * Example VATSIM SSO Script package.
+ * Configuration file for OAuth integration
+ * 
+ * @author Kieran Hardern
+ * @version 0.2
  */
+
+/*
+ * Contains all temporary config variables
+ */
+$sso = array();
 
 /*
  * The location of the VATSIM OAuth interface
  */
-$base = 'https://cert.vatsim.net/sso/';
+$sso['base'] = 'https://cert.vatsim.net/sso/';
 
 /*
  * The consumer key for your organisation (provided by VATSIM)
  */
-$key = 'SKYEVENT01';
+$sso['key'] = 'SKYEVENT01';
 
 /*
- * The secret key for your organisation (provided by VATSIM)
+ * The secret key for your orgnisation (provided by VATSIM)
  * Do not give this to anyone else or display it to your users. It must be kept server-side
  */
-$secret = 'ZIEV4Flhmj2_~1.-oQCO';
+$sso['secret'] = 'ZIEV4Flhmj2_~1.-oQCO';
+
+/*
+ * The key for whic temporary (token) details for each user will be stored e.g. $_SESSION['mykey']
+ * If you chose to handle the tokens yourself by another method, you can remove this
+ */
+define('SSO_SESSION', 'oauth');
+
+/*
+ * The URL users will be redirected to after they log in, this should
+ * be on the same server as the request
+ */
+
+// Using https or http?
+$http = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']) ? 'https://' : 'http://';
+
+// determing location from URL (comment out if manually defining - example below)
+$sso['return'] = $http.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'].'?return';
+//$sso_return =  'http://example.com/index.php?return';
 
 /*
  * The signing method you are using to encrypt your request signature.
  * Different options must be enabled on your account at VATSIM.
  * Options: RSA / HMAC
  */
-$method = 'HMAC';
+$sso['method'] = 'HMAC';
 
 /*
  * Your RSA **PRIVATE** key
  * If you are not using RSA, this value can be anything (or not set)
  */
-$cert = '';
+$sso['cert'] = <<<EOD
+-----BEGIN RSA PRIVATE KEY-----
+-----END RSA PRIVATE KEY-----
+EOD;
 
-/*
- * The URL users will be redirected to after they log in, this should
- * be on the same server as the request
- */
-$return = 'http://fuck.io/User/login';
+
+?>
